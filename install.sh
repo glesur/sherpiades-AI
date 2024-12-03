0bb14407cb8aac0ebcfdd22ddf59aa0d4fddfbaa
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2024-12-03 13:57:04 +0100

    force update pip in venv

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ echo "*******************************"
 
 python3 -m venv ./env
 source env/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "*******************************"
